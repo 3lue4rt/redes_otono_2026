c4db46e8f6d61e570afa5391a3e5ce1c4062fab5
Benjamin Duarte <bduartenass@gmail.com>
2026-03-23 16:43:14 -0300

    Avance act1 pt1 p1

---
+++ control_1/server.py
@@ -0,0 +1,27 @@
+import socket
+
+#CONSTANTES
+IP_VM = "192.168.122.1"
+port = 80
+vm_address = (IP_VM, port)
+buffer_size = 1024
+
+print('Creando socket - Servidor ...')
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("binding ...")
+server_socket.bind(vm_address)
+
+print("escuchando hasta 3")
+server_socket.listen(3)
+
+while True:
+    new_socket, new_socket_address = server_socket.accept()
+
+    recv_message = new_socket.recv(buffer_size)
+
+    if recv_message.decode()=="exit":
+        new_socket.close()
+        print(f"conexión con {new_socket_address} ha sido cerrada")
+
+    print(f' -> Se ha recibido el siguiente mensaje: {recv_message}')
